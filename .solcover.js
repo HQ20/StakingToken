28cab8fc62804a5add636a40349ff3c1ca3380cd
Alberto Cuesta <alberto@techhq.io>
2019-03-25 09:53:40 +0000

    Added development config files

---
+++ .solcover.js
@@ -0,0 +1,12 @@
+module.exports = {
+    skipFiles: [
+        "./contracts/test/SampleDetailedERC20.sol",
+        "./contracts/test/SampleERC721.sol",
+        "./contracts/test/SamplePlainERC20.sol",
+        "./contracts/mocks/BILDDataTest.sol",
+        "./contracts/mocks/FeesMock.sol",
+        "./contracts/mocks/FixidityLibMock.sol",
+        "./contracts/mocks/LogarithmLibMock.sol",
+        "./contracts/mocks/UtilsLibMock.sol",
+    ]
+};
